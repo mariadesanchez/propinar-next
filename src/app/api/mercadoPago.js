463fc22a54a6cfba78a9b5fecb8be9808143925c
mariadesanchez <calder07@gmail.com>
2024-03-05 17:07:54 -0300

    mercadoPago

--- src/app/api/mercadoPago.js
+++ src/app/api/mercadoPago.js
@@ -5,6 +5,9 @@ mercadopago.configure({
 });
 
 export default async function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://propinar-next.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     if (req.method === 'POST') {
         try {
             const { items, shipment_cost } = req.body;
